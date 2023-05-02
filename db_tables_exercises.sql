f627a2910b76b36b7fdb9c0a15b7be6cdfb32d9f
Scott Mattes <s.scott.mattes@gmail.com>
2023-05-02 11:48:02 -0500

    updated basic statements and tables exercises

--- db_tables_exercises.sql
+++ db_tables_exercises.sql
@@ -14,3 +14,4 @@ SHOW TABLES;
 -- 14) there are no shared columns or relationships between the employees and departments columns
 SHOW CREATE TABLE dept_manager;
 -- CREATE TABLE'dept_manager', 'CREATE TABLE `dept_manager` (\n  `emp_no` int NOT NULL,\n  `dept_no` char(4) NOT NULL,\n  `from_date` date NOT NULL,\n  `to_date` date NOT NULL,\n  PRIMARY KEY (`emp_no`,`dept_no`),\n  KEY `dept_no` (`dept_no`),\n  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE ON UPDATE RESTRICT,\n  CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE ON UPDATE RESTRICT\n) ENGINE=InnoDB DEFAULT CHARSET=latin1'
+DESCRIBE employees;
